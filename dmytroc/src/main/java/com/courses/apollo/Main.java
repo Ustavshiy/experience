f8238e7e374f7a8fcbe67c0b02813933dfe17ec5
dmytroc <chopenko.d@gmail.com>
2017-02-20 21:09:58 +0200

    AP-6 I created public static void main method. Changed Main class to final."
    This reverts commit 912b99d474ef3930e760b82c8228f766ae6a07c0.

--- dmytroc/src/main/java/com/courses/apollo/Main.java
+++ dmytroc/src/main/java/com/courses/apollo/Main.java
@@ -3,18 +3,5 @@ package com.courses.apollo;
 /**
  * Created by User on 20.02.2017.
  */
-public final class Main {
-
-    private Main() {
-    }
-
-    /**
-     * This is main method.
-     *
-     * @param args strings array
-     */
-
-    public static void main(String[] args) {
-    }
-
+public class Main {
 }
