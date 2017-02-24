5cbd4c5ff05cf9ce23921bd16de7dc5921397934
Dmytro Chopenko <chopenko.d@gmail.com>
2017-02-24 15:31:02 +0200

    AP-18 I Added to Main class  in module dmytroc an array and method moduleSevenAndFive. It prints in console all numbers divides by 7 and 5.

--- dmytroc/src/main/java/com/courses/apollo/Main.java
+++ dmytroc/src/main/java/com/courses/apollo/Main.java
@@ -4,13 +4,38 @@ package com.courses.apollo;
  * Created by User on 20.02.2017.
  */
 public final class Main {
-    private Main() {}
+    private Main() {
+    }
 
     /**
      * This is main method.
      *
      * @param args strings array
      */
-    public static void  main(String[] args) {
+    public static void main(String[] args) {
+
+
+        //Test array "array" declared and initialized
+
+        final int[] array = {3, 35, 14, 87, 70, 57, 105, 106, 10, 140};
+
+
+        //Call method "moduleSevenAndFive" with test array "array"
+
+        Main.moduleSevenAndFive(array);
+    }
+
+    /*
+     * This method "moduleSevenAndFive" print in console all integers in array,
+     * that can be divided by 5 and 7.
+     */
+    static void moduleSevenAndFive(int[] arr) {
+        final int divideFirst = 5;
+        final int divideSecond = 7;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % divideFirst == 0 & arr[i] % divideSecond == 0) {
+                System.out.print(arr[i] + " ");
+            }
+        }
     }
 }
