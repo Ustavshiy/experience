145da00ee0fdf0c3f72a435c3762be8e07e90f78
Alan Kozko <alan_falan@mail.ru>
2017-02-24 18:01:48 +0200

    Create module and class Main

---
+++ alank/src/main/java/com/courses/apollo/Main.java
@@ -0,0 +1,13 @@
+package com.courses.apollo;
+public final class Main {
+    private Main(){
+    }
+
+    /**
+     * This is main method.
+     * *
+     * @param args  strings array
+     */
+    public static void main(String[] args) {
+    }
+}
