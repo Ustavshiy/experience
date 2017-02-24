24211babc9ce5e935e00e117bc2ee7d8a1a94025
Dmytro Chopenko <chopenko.d@gmail.com>
2017-02-24 17:41:35 +0200

    AP-18 Changed comments. Changed names of variables and method.

--- dmytroc/src/main/java/com/courses/apollo/Main.java
+++ dmytroc/src/main/java/com/courses/apollo/Main.java
@@ -14,26 +14,27 @@ public final class Main {
      */
     public static void main(String[] args) {
 
-
-        //Test array "array" declared and initialized
-
+        /**
+         * Test array "array" declared and initialized.
+         */
         final int[] array = {3, 35, 14, 87, 70, 57, 105, 106, 10, 140};
 
+        /**
+         * Call method "moduleSevenAndFive" with test array "array".
+         */
 
-        //Call method "moduleSevenAndFive" with test array "array"
-
-        Main.moduleSevenAndFive(array);
+        Main.printModuloByTwoNumbers(array);
     }
 
-    /*
+    /**
      * This method "moduleSevenAndFive" print in console all integers in array,
      * that can be divided by 5 and 7.
      */
-    static void moduleSevenAndFive(int[] arr) {
-        final int divideFirst = 5;
-        final int divideSecond = 7;
+    static void printModuloByTwoNumbers(int[] arr) {
+        final int moduloOne = 5;
+        final int moduloTwo = 7;
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] % divideFirst == 0 && arr[i] % divideSecond == 0) {
+            if (arr[i] % moduloOne == 0 && arr[i] % moduloTwo == 0) {
                 System.out.print(arr[i] + " ");
             }
         }
