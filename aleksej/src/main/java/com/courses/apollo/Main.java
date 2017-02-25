48297ba047a4c1e304b57ad6ef4f2e77cec2e226
kolomietc <aleksej.kolomietc@gmail.com>
2017-02-25 14:55:19 +0200

    AP-15 I change method.

--- aleksej/src/main/java/com/courses/apollo/Main.java
+++ aleksej/src/main/java/com/courses/apollo/Main.java
@@ -23,19 +23,14 @@ public final class Main {
      * @param arr int array
      */
     static void printEvenAndOddNumbers(int[] arr) {
-        System.out.println("Even numbers: ");
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] % 2 == 0) {
-                System.out.print(arr[i] + " ");
-            }
-        }
-        System.out.println();
-        System.out.println("Odd numbers: ");
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] % 2 != 0) {
-                System.out.print(arr[i] + " ");
+                System.out.println(arr[i] + " even");
+            } else {
+                System.out.println(arr[i] + " odd");
             }
 
         }
+
     }
 }
