195f54f6097750e8eb02ae9981e3fb0a3369d498
MashaJM <masha.chip@gagadget.com>
2017-02-25 23:21:21 +0200

    Array's Max&Min the most impressive ASD methods ever

--- masha/src/main/java/com/courses/apollo/Main.java
+++ masha/src/main/java/com/courses/apollo/Main.java
@@ -1,7 +1,7 @@
 package com.courses.apollo;
 
 /**
- * Created by User on 20.02.2017.
+ * Created by Masha on 20.02.2017.It's test
  */
 public final class Main {
 
@@ -14,7 +14,34 @@ public final class Main {
      * @param args strings array
      */
     public static void main(String[] args) {
+        int[] array = {2, 100, 50 , 1,7, -10};
+        System.out.println(min(array));
+        System.out.println(max(array));
     }
+
+     private static int min (int[] array) {
+      int min = array[0];
+      for (int i = 1; i < array.length; i++) {
+          if (min > array[i]) {
+              min = array[i];
+          }
+      }
+
+    return min;
+    }
+
+    private static int max (int[] array) {
+        int max = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (max < array[i]) {
+                max = array[i];
+            }
+        }
+
+        return max;
+    }
+
+
 }
 
 
