78a7073a6cdc65d0ad26be59aee839ac6bf0f378
kolomietc <aleksej.kolomietc@gmail.com>
2017-02-25 14:15:22 +0200

    AP-15 I created method prints even and odd numbers in console.

--- aleksej/src/main/java/com/courses/apollo/Main.java
+++ aleksej/src/main/java/com/courses/apollo/Main.java
@@ -13,5 +13,29 @@ public final class Main {
      * @param args strings array
      */
     public static void main(String[] args) {
+        final int[] array = {12, 15, 28, 33, 49, 6};
+        Main.printEvenAndOddNumbers(array);
+    }
+
+    /**
+     * This method prints even and odd numbers in console.
+     *
+     * @param arr int array
+     */
+    static void printEvenAndOddNumbers(int[] arr) {
+        System.out.println("Even numbers: ");
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 == 0) {
+                System.out.print(arr[i] + " ");
+            }
+        }
+        System.out.println();
+        System.out.println("Odd numbers: ");
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 != 0) {
+                System.out.print(arr[i] + " ");
+            }
+
+        }
     }
 }
