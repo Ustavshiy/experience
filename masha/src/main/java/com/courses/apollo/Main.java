31b56746beaf6e6f2b453404b3ead475e17abdd7
MashaJM <masha.chip@gagadget.com>
2017-02-26 13:45:05 +0200

    Array's Max&Min the most impressive ASD methods ever for my Home Work

--- masha/src/main/java/com/courses/apollo/Main.java
+++ masha/src/main/java/com/courses/apollo/Main.java
@@ -23,7 +23,6 @@ public final class Main {
       int min = array[0];
       for (int i = 1; i < array.length; i++) {
           if (min > array[i]) {
-              
               min = array[i];
           }
       }
