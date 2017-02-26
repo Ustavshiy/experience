64403e956c8df64423aa3f33abcb8093dc29dda5
pronko.igor <pronko.igor@gmail.com>
2017-02-27 00:52:04 +0200

    Changed  integer "n" to "array.lenght" Changed method "SortUpArray(array, int i)" to "SortUpArray(array) Changed method "SortDownArray(array, int i)" to "SortDownArray(array)

--- ihorp/src/main/java/com/courses/apollo/Main.java
+++ ihorp/src/main/java/com/courses/apollo/Main.java
@@ -2,7 +2,7 @@ package com.courses.apollo;
 
 /**
  * Created by Ihor on 26.02.2017.
- * Programm sort an array of integer and input ascending order and descending order.
+ * Programm sort an array of integer and input ascending order and descending order
  */
 
 public class Main {
@@ -10,18 +10,15 @@ public class Main {
     //Create an array
     static int[] array = {34, -56, 3456, 1, 0, 43, -2345, 84, -23, -1};
 
-    // Int n tell us how many integers we have into array
-    static int n = array.length;
-
     public static void main(String[] args) {
         //Print sort array UP
         System.out.println("Output array sorted up:");
-        for (int v: Sort.SortUpArray(array, n)) {
+        for (int v: Sort.SortUpArray(array)) {
             System.out.println(v);
         }
         // Print Sort array DOWN
         System.out.println("\n\nOutput array sorted down:");
-        for (int v: Sort.SortDownArray(array, n)) {
+        for (int v: Sort.SortDownArray(array)) {
             System.out.println(v);
         }
     }
--- ihorp/src/main/java/com/courses/apollo/Sort.java
+++ ihorp/src/main/java/com/courses/apollo/Sort.java
@@ -1,5 +1,7 @@
 package com.courses.apollo;
 
+import static com.courses.apollo.Main.array;
+
 /**
  * Created by User on 26.02.2017.
  * This method sorts the array in ascending and descending order
@@ -8,12 +10,12 @@ public class Sort {
     private Sort() {
     }
     //Bubble sort UP
-    static int[] SortUpArray (int array[], int n) {
+    static int[] SortUpArray (int array[]) {
         // int i is a counter permutations in a bubble sort.
         int i;
         do {
             i = 0;
-            for (int a = 1; a < n; a++) {
+            for (int a = 1; a < array.length; a++) {
                 if (array[a - 1] > array[a]) {
                     int b = array[a - 1];
                     array[a - 1] = array[a];
@@ -22,17 +24,19 @@ public class Sort {
                 }
             }
         }
+        /* i = 0 and the loop is end when the loop is repeated "arrey.lenght" times
+        and the condition in line 19 never return "TRUE"*/
         while (i > 0);
         return array;
     }
 
     //Bubble sort DOWN
-    static int[] SortDownArray (int array[], int n) {
+    static int[] SortDownArray (int array[]) {
         // int i is a counter permutations in a bubble sort.
         int i;
         do {
             i = 0;
-            for (int a = 1; a < n; a++) {
+            for (int a = 1; a < array.length; a++) {
                 if (array[a - 1] < array[a]) {
                     int b = array[a - 1];
                     array[a - 1] = array[a];
@@ -41,6 +45,8 @@ public class Sort {
                 }
             }
         }
+        /* i = 0 and the loop is end when the loop is repeated "arrey.lenght" times
+        and the condition in line 19 never return "TRUE"*/
         while (i > 0);
         return array;
     }
