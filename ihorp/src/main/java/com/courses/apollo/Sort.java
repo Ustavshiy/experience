ce0f0dd229576e6c21f895f0739025fbce8d9719
pronko.igor <pronko.igor@gmail.com>
2017-02-26 23:56:09 +0200

    Add file Sort

---
+++ ihorp/src/main/java/com/courses/apollo/Sort.java
@@ -0,0 +1,45 @@
+package com.courses.apollo;
+
+/**
+ * Created by User on 26.02.2017.
+ * This method sorts the array in ascending and descending order
+ */
+public class Sort {
+    private Sort() {
+    }
+    //Bubble sort UP
+    static int[] SortUpArray (int array[], int n) {
+        int i;
+        do {
+            i = 0;
+            for (int a = 1; a < n; a++) {
+                if (array[a - 1] > array[a]) {
+                    int b = array[a - 1];
+                    array[a - 1] = array[a];
+                    array[a] = b;
+                    i++;
+                }
+            }
+        }
+        while (i > 0);
+        return array;
+    }
+
+    //Bubble sort DOWN
+    static int[] SortDownArray (int array[], int n) {
+        int i;
+        do {
+            i = 0;
+            for (int a = 1; a < n; a++) {
+                if (array[a - 1] < array[a]) {
+                    int b = array[a - 1];
+                    array[a - 1] = array[a];
+                    array[a] = b;
+                    i++;
+                }
+            }
+        }
+        while (i > 0);
+        return array;
+    }
+}
