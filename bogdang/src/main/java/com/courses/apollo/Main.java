c40bda9e7f841654a92f51f4878c6a4c903b9db0
Maestro <maestrobsm@gmail.com>
2017-02-26 21:43:23 +0200

    AP-19 Add final version

--- bogdang/src/main/java/com/courses/apollo/Main.java
+++ bogdang/src/main/java/com/courses/apollo/Main.java
@@ -14,5 +14,25 @@ public final class Main {
      * @param args strings array
      */
     public static void main(String[] args) {
+        final int[] mass = {5, -32, 2, 23, 4, 299, 3, 0, 33, -126, -6, 1};
+
+        sort(mass);
+
+        for (Integer n : mass) {
+            System.out.print(n + ",");
+        }
+    }
+
+    static int[] sort(int[] mass) {
+        for (int i = mass.length - 1; i >= 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (Math.abs(mass[j]) < Math.abs(mass[j + 1])) {
+                    int t = mass[j];
+                    mass[j] = mass[j + 1];
+                    mass[j + 1] = t;
+                }
+            }
+        }
+        return mass;
     }
 }
