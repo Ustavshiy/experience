43174f1541df1b387d7d98115a9cce96ac903c28
pronko.igor <pronko.igor@gmail.com>
2017-02-26 23:39:27 +0200

    Create a Sort class

--- ihorp/src/main/java/com/courses/apollo/Main.java
+++ ihorp/src/main/java/com/courses/apollo/Main.java
@@ -1,18 +1,28 @@
 package com.courses.apollo;
 
 /**
- * Created by User on 20.02.2017.
+ * Created by Ihor on 26.02.2017.
+ * Programm sort an array of integer and input ascending order and descending order
  */
-public final class Main {
 
-    private Main() {}
+public class Main {
 
-    /**
-     * This is main method.
-     * @param args strings array
-     * Test commit to push
-     */
+    //Create an array
+    static int[] array = {34, -56, 3456, 1, 0, 43, -2345, 84, -23, -1};
+
+    // Int n tell us how many integers we have into array
+    static int n = array.length;
+
     public static void main(String[] args) {
-
+        //Print sort array UP
+        System.out.println("Output array sorted up:");
+        for (int v: Sort.SortUpArray(array, n)) {
+            System.out.println(v);
+        }
+        // Print Sort array DOWN
+        System.out.println("\n\nOutput array sorted down:");
+        for (int v: Sort.SortDownArray(array, n)) {
+            System.out.println(v);
+        }
     }
 }
