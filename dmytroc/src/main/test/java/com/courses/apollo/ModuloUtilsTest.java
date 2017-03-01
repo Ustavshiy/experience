806237f4404100fbfc8fefcc6d85b98b074d04f2
Dmytro Chopenko <chopenko.d@gmail.com>
2017-03-01 19:41:18 +0200

    AP-28 Joined all Asserts into one method. Arguments moved to field of ModuloUtilsTest class.

--- dmytroc/src/main/test/java/com/courses/apollo/ModuloUtilsTest.java
+++ dmytroc/src/main/test/java/com/courses/apollo/ModuloUtilsTest.java
@@ -8,43 +8,22 @@ import org.junit.Test;
  */
 public class ModuloUtilsTest {
 
-    @Test
-    public void printModuloBySevenAndFiveWithBothTrueTest() {
-        final int[] TestArray = {140, 3, 35, 14, 35, 87, 70, 57, 105, 106, 10};
-        final int moduloOne = 5;
-        final int moduloTwo = 7;
-        int[] receivedResult = ModuloUtils.printModuloByTwoNumbers(TestArray, moduloOne, moduloTwo);
-        final int[] expectedResult = {140, 35, 35, 70, 105};
-        Assert.assertArrayEquals(expectedResult, receivedResult);
-    }
+    private int[] ArrayWithBothTrue = {140, 3, 35, 14, 35, 87, 70, 57, 105, 106, 10};
+    private int[] ArrayWithBothFalse = {12, 3, 14, 22, 87, 71, 57, 106, 109};
+    private int[] ArrayWithOnlyFiveIsTrue = {12, 3, 15, 22, 87, 75, 57, 106, 109};
+    private int[] ArrayWithOnlySevenIsTrue = {12, 3, 14, 22, 21, 75, 57, 49, 109};
+
+    private int[] expectedResultWithBothTrue = {140, 35, 35, 70, 105};
+    private int[] expectedResultWithFalse = {};
+
+    private int moduloOne = 5;
+    private int moduloTwo = 7;
 
     @Test
-    public void printModuloBySevenAndFiveWithBothFalseTest() {
-        final int[] TestArray = {12, 3, 14, 22, 87, 71, 57, 106, 109};
-        final int moduloOne = 5;
-        final int moduloTwo = 7;
-        int[] receivedResult = ModuloUtils.printModuloByTwoNumbers(TestArray, moduloOne, moduloTwo);
-        final int[] expectedResult = {};
-        Assert.assertArrayEquals(expectedResult, receivedResult);
-    }
-
-    @Test
-    public void printModuloBySevenAndFiveWithOnlyFiveIsTrueTest() {
-        final int[] TestArray = {12, 3, 15, 22, 87, 75, 57, 106, 109};
-        final int moduloOne = 5;
-        final int moduloTwo = 7;
-        int[] receivedResult = ModuloUtils.printModuloByTwoNumbers(TestArray, moduloOne, moduloTwo);
-        final int[] expectedResult = {};
-        Assert.assertArrayEquals(expectedResult, receivedResult);
-    }
-
-    @Test
-    public void printModuloBySevenAndFiveWithOnlySevenIsTrueTest() {
-        final int[] TestArray = {12, 3, 14, 22, 21, 75, 57, 49, 109};
-        final int moduloOne = 5;
-        final int moduloTwo = 7;
-        int[] receivedResult = ModuloUtils.printModuloByTwoNumbers(TestArray, moduloOne, moduloTwo);
-        final int[] expectedResult = {};
-        Assert.assertArrayEquals(expectedResult, receivedResult);
+    public void printModuloBySevenAndFiveTest() {
+        Assert.assertArrayEquals(expectedResultWithBothTrue, ModuloUtils.printModuloByTwoNumbers(ArrayWithBothTrue, moduloOne, moduloTwo));
+        Assert.assertArrayEquals(expectedResultWithFalse, ModuloUtils.printModuloByTwoNumbers(ArrayWithBothFalse, moduloOne, moduloTwo));
+        Assert.assertArrayEquals(expectedResultWithFalse, ModuloUtils.printModuloByTwoNumbers(ArrayWithOnlyFiveIsTrue, moduloOne, moduloTwo));
+        Assert.assertArrayEquals(expectedResultWithFalse, ModuloUtils.printModuloByTwoNumbers(ArrayWithOnlySevenIsTrue, moduloOne, moduloTwo));
     }
 }
