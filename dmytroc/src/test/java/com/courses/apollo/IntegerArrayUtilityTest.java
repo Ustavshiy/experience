87d49e4809b87f3e21beebfec62c163d511e9adb
Dmytro Chopenko <chopenko.d@gmail.com>
2017-03-03 18:06:02 +0200

    AP-28 Moved arguments declaration from method to fields in IntegerArrayUtilityTest

--- dmytroc/src/test/java/com/courses/apollo/IntegerArrayUtilityTest.java
+++ dmytroc/src/test/java/com/courses/apollo/IntegerArrayUtilityTest.java
@@ -11,11 +11,11 @@ public class IntegerArrayUtilityTest {
     IntegerArrayUtility integerArrayUtility = new IntegerArrayUtility();
     final int[] testArray = {23432566, 23144, 456, 4544544, 326, 2345654};
     final int[] testNullArray = {};
+    final int expectedResult = 4544544;
+    final int expectedResultNull = 0;
 
     @Test
     public void minDifferentDIgitsFinderTest() {
-        final int expectedResult = 4544544;
-        final int expectedResultNull = 0;
         int receivedResult = integerArrayUtility.minDifferentDigitsFinder(testArray);
         int receivedResultNull = integerArrayUtility.minDifferentDigitsFinder(testNullArray);
         Assert.assertEquals(expectedResult, receivedResult);
