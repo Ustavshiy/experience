402c552e4e6c5e629a4d1c94d19c2b62e679a17f
Maestro <maestrobsm@gmail.com>
2017-03-06 11:38:37 +0200

    AP -29 Add class sortOddEven correct

--- bogdang/src/test/java/com/courses/apollo/SortEvenNumbersTest.java
+++ bogdang/src/test/java/com/courses/apollo/SortEvenNumbersTest.java
@@ -10,8 +10,8 @@ public class SortEvenNumbersTest {
 
     @Test
     public void sortEvenTest() {
-        final int[] arrayUnSorted = {12, 53, 5678, 24, 43, 32};
-        final int[] sortedArray = {12, 5678, 24, 32};
+        final int[] arrayUnSorted = {12, 53, 5678, 24, 43, 32, 10};
+        final int[] sortedArray = {12, 5678, 24, 32, 10};
         final int[] afterMethodRun = SortEvenNumbers.sortEven(arrayUnSorted);
         Assert.assertArrayEquals(sortedArray, afterMethodRun);
 
@@ -19,8 +19,8 @@ public class SortEvenNumbersTest {
 
     @Test
     public void sortOddEvenTest() {
-        final int[] arrayUnSorted = {12, 5678, 24, 32};
-        final int[] sortedArray = {12, 5678, 32};
+        final int[] arrayUnSorted = {12, 5678, 24, 32, 10};
+        final int[] sortedArray = {12, 5678, 32, 10};
         final int[] afterMethodRun = SortEvenNumbers.sortOddEven(arrayUnSorted);
         Assert.assertArrayEquals(sortedArray, afterMethodRun);
 
