2c39c1f411322c785e5861b033546fdda568114e
Dmytro Chopenko <chopenko.d@gmail.com>
2017-03-08 12:15:47 +0200

    AP-37 Changed constructor declaring to set method declaring for field variables in Fraction.

--- dmytroc/src/main/java/com/courses/apollo/FractionUtils.java
+++ dmytroc/src/main/java/com/courses/apollo/FractionUtils.java
@@ -19,12 +19,16 @@ public class FractionUtils {
         Fraction[] resultArray = new Fraction[(inputArray.length + 1) / 2];
         for (int i = 0; i < inputArray.length; i++) {
             if (i % 2 == 0 && i != inputArray.length - 1) {
-                resultArray[counter] = new Fraction(inputArray[i].getNumerator() + inputArray[i + 1].getNumerator(),
-                        inputArray[i].getDenominator() + inputArray[i + 1].getDenominator());
+                resultArray[counter] = new Fraction();
+                resultArray[counter].setNumerator(inputArray[i].getNumerator()
+                        + inputArray[i + 1].getNumerator());
+                resultArray[counter].setDenominator(inputArray[i].getDenominator()
+                        + inputArray[i + 1].getDenominator());
                 counter++;
             } else if (i == inputArray.length - 1) {
-                resultArray[counter] = new Fraction(inputArray[i].getNumerator(),
-                        inputArray[i].getDenominator());
+                resultArray[counter] = new Fraction();
+                resultArray[counter].setNumerator(inputArray[i].getNumerator());
+                resultArray[counter].setDenominator(inputArray[i].getDenominator());
                 counter++;
             }
         }
