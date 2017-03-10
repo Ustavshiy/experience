72ee24a2218bb5d56c1189a8f941f838f8bcfa0a
Maestro <maestrobsm@gmail.com>
2017-03-10 14:36:29 +0200

    AP-38 Add 9-houses

--- bogdang/src/main/java/com/courses/apollo/model/House.java
+++ bogdang/src/main/java/com/courses/apollo/model/House.java
@@ -26,6 +26,10 @@ public class House {
 
     }
 
+
+
+
+
     public House(Integer Id, Integer num, Integer area,
                  Integer floor, Integer numRooms, String street, String bildType, String lifeTime) {
         houseId = Id;
--- bogdang/src/test/java/com/courses/apollo/HouseTestData.java
+++ bogdang/src/test/java/com/courses/apollo/HouseTestData.java
@@ -5,11 +5,19 @@ import com.courses.apollo.model.House;
 public class HouseTestData {
 
     public static House[] getHouses() {
-        House house = new House(1,4, 35,5, 2, "Borshagovskaya",
-                "Apartment", "4 years");
-        House houseTwo = new House(2,1,55,1, 5, "Shevchenka",
-                "private house", "2 years");
-        return new House[]{house, houseTwo};
+        House house = new House(1,4, 35,5, 2, "Borshagovskaya","Apartment", "4 years");
+        House houseTwo = new House(2,1,55,1, 5, "Shevchenka","private house", "2 years");
+        House houseThree = new House(3,2,60,4, 4, "Bobenka","private house", "1 years");
+        House houseFour = new House(4,10,40,6, 3, "Pavlova","Apartment", "5 years");
+        House houseFive = new House(5,7,25,2, 2, "Schorsa","Apartment", "8 years");
+        House houseSix = new House(6,52,60,10, 3, "Lepse","Apartment", "15years");
+        House houseSeven = new House(7,126,80,8, 5, "Bobrova","Apartment", "12 years");
+        House houseEight = new House(8,56,45,1, 3, "Franka","private house", "20 years");
+        House houseNine= new House(9, 78,30,3, 2, "Sobka","Apartment", "18 years");
+
+
+        return new House[]{house, houseTwo, houseThree, houseFour, houseFive, houseSix, houseSeven, houseEight, houseNine};
+
 
     }
 }
