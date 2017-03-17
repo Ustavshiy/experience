714edab551ff6a6edba690e0787657d1cc4c02ed
MashaJM <masha.chip@gagadget.com>
2017-03-17 02:27:15 +0200

    first draft of Patient class for HW3

--- masha/src/main/java/com/courses/apollo/model/MedicalInfo.java
+++ masha/src/main/java/com/courses/apollo/model/MedicalInfo.java
@@ -7,5 +7,24 @@ public class MedicalInfo {
     private Integer MedicalCardNumber;
     private String diagnosis;
 
+    public MedicalInfo(Integer medicalCardNumber, String diagnosis) {
+        MedicalCardNumber = medicalCardNumber;
+        this.diagnosis = diagnosis;
+    }
 
+    public Integer getMedicalCardNumber() {
+        return MedicalCardNumber;
+    }
+
+    public void setMedicalCardNumber(Integer medicalCardNumber) {
+        MedicalCardNumber = medicalCardNumber;
+    }
+
+    public String getDiagnosis() {
+        return diagnosis;
+    }
+
+    public void setDiagnosis(String diagnosis) {
+        this.diagnosis = diagnosis;
+    }
 }
--- masha/src/main/java/com/courses/apollo/model/Patient.java
+++ masha/src/main/java/com/courses/apollo/model/Patient.java
@@ -6,5 +6,40 @@ package com.courses.apollo.model;
 public class Patient {
 
     private Integer patientId;
+    private PersonalInfo personalInfo;
+    private MedicalInfo medicalInfo;
 
+    public Patient(Integer patientId, PersonalInfo personalInfo, MedicalInfo medicalInfo) {
+        this.patientId = patientId;
+        this.personalInfo = personalInfo;
+        this.medicalInfo = medicalInfo;
+    }
+
+    public Patient(Integer patientId) {
+        this.patientId = patientId;
+    }
+
+    public Integer getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(Integer patientId) {
+        this.patientId = patientId;
+    }
+
+    public PersonalInfo getPersonalInfo() {
+        return personalInfo;
+    }
+
+    public void setPersonalInfo(PersonalInfo personalInfo) {
+        this.personalInfo = personalInfo;
+    }
+
+    public MedicalInfo getMedicalInfo() {
+        return medicalInfo;
+    }
+
+    public void setMedicalInfo(MedicalInfo medicalInfo) {
+        this.medicalInfo = medicalInfo;
+    }
 }
