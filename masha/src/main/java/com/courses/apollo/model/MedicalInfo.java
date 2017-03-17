b3352b28ce2c4b1a3b57332cc50a169077f2535e
MashaJM <masha.chip@gagadget.com>
2017-03-17 02:20:49 +0200

    first draft of Patient class for HW3

---
+++ masha/src/main/java/com/courses/apollo/model/MedicalInfo.java
@@ -0,0 +1,11 @@
+package com.courses.apollo.model;
+
+/**
+ * Created by MashaJM on 17.03.2017.
+ */
+public class MedicalInfo {
+    private Integer MedicalCardNumber;
+    private String diagnosis;
+
+
+}
---
+++ masha/src/main/java/com/courses/apollo/model/PersonalInfo.java
@@ -0,0 +1,76 @@
+package com.courses.apollo.model;
+
+/**
+ * Created by MashaJM on 17.03.2017.
+ */
+public class PersonalInfo {
+
+    private String patientFamilyName;
+    private String patientName;
+    private String patientPatronymicName;
+    private String patientAddress;
+    private String patientPhoneNumber;
+
+
+
+
+    public PersonalInfo() {
+    }
+
+    public PersonalInfo(String patientFamilyName, String patientName, String patientPatronymicName, String patientAddress, String patientPhoneNumber) {
+        this.patientFamilyName = patientFamilyName;
+        this.patientName = patientName;
+        this.patientPatronymicName = patientPatronymicName;
+        this.patientAddress = patientAddress;
+        this.patientPhoneNumber = patientPhoneNumber;
+    }
+
+    public String getPatientFamilyName() {
+        return patientFamilyName;
+    }
+
+    public void setPatientFamilyName(String patientFamilyName) {
+        this.patientFamilyName = patientFamilyName;
+    }
+
+    public String getPatientName() {
+        return patientName;
+    }
+
+    public void setPatientName(String patientName) {
+        this.patientName = patientName;
+    }
+
+    public String getPatientPatronymicName() {
+        return patientPatronymicName;
+    }
+
+    public void setPatientPatronymicName(String patientPatronymicName) {
+        this.patientPatronymicName = patientPatronymicName;
+    }
+
+    public PersonalInfo(String patientFamilyName, String patientName) {
+        this.patientFamilyName = patientFamilyName;
+        this.patientName = patientName;
+    }
+
+    public String getPatientAddress() {
+        return patientAddress;
+    }
+
+    public void setPatientAddress(String patientAddress) {
+        this.patientAddress = patientAddress;
+    }
+
+    public String getPatientPhoneNumber() {
+        return patientPhoneNumber;
+    }
+
+    public void setPatientPhoneNumber(String patientPhoneNumber) {
+        this.patientPhoneNumber = patientPhoneNumber;
+    }
+}
+
+
+
+
