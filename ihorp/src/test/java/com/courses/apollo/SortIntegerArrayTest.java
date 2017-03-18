ec887bd8a5b4df5278e09169d001f8cff0fd7883
pronko.igor <pronko.igor@gmail.com>
2017-03-18 15:49:01 +0200

    Revert changes

---
+++ ihorp/src/test/java/com/courses/apollo/SortIntegerArrayTest.java
@@ -0,0 +1,30 @@
+package com.courses.apollo;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by Ihor Pronko.
+ */
+
+public class SortIntegerArrayTest {
+
+    /**
+     * Create arrays for tests
+     */
+    private int[] testArray = {128, -1, -48, 798, 89, 0, -78};
+    private int[] arrayUp = {-78, -48, -1, 0, 89, 128, 798};
+    private int[] arrayDown = {798, 128, 89, 0, -1, -48, -78};
+    SortIntegerArray SortIntegerArray = new SortIntegerArray();
+
+    @Test
+    public void sortUpArrayTest() {
+        Assert.assertArrayEquals (arrayUp, SortIntegerArray.sortAscending(testArray));
+    }
+
+    @Test
+    public void sortDownArrayTest() {
+        Assert.assertArrayEquals(arrayDown, SortIntegerArray.sortDescending(testArray));
+    }
+
+}
