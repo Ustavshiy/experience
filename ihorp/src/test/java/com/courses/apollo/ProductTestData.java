a9bd8a39929c690e52754a6a9cacb9167f119b50
pronko.igor <pronko.igor@gmail.com>
2017-03-18 15:56:49 +0200

    Revert changes in old home work.
    Move fields in class ProductTestData into method.

--- ihorp/src/test/java/com/courses/apollo/ProductTestData.java
+++ ihorp/src/test/java/com/courses/apollo/ProductTestData.java
@@ -1,88 +1,91 @@
 package com.courses.apollo;
 
+import com.courses.apollo.model.Product;
+
 import java.util.ArrayList;
 
 public class ProductTestData {
 
-    public static ArrayList<Product> getProductArrayList() {
+    public static ArrayList<Product> getProducts() {
 
         Product productOne = new Product(
-                1,
+                0,
                 "Tomato",
                 482862443,
                 "MyGrandma (R)",
-                10.5,
+                "10.5",
                 120,
                 2);
         Product productTwo = new Product(
-                2,
+                1,
                 "Orange",
                 482587654,
                 "YourGrandpa (R)",
-                15.68,
+                "15.68",
                 200,
                 6);
         Product productThree = new Product(
-                3,
+                2,
                 "Potato",
                 482658745,
                 "Lukashenko (R)",
-                0.68,
+                "0.68",
                 365,
                 10);
         Product productFour = new Product(
-                4,
+                3,
                 "Banana",
                 482587489,
                 "BestNiggasFruits (R)",
-                20.4,
+                "20.46",
                 50,
                 5);
         Product productFive = new Product(
-                5,
+                4,
                 "Milk",
                 482541247,
                 "Yahotinske (R)",
-                8.68,
+                "8.68",
                 10,
                 1);
         Product productSix = new Product(
-                6,
-                "Bacon",
+                5,
+                "Banana",
                 482658713,
-                "Meat (R)",
-                70.57,
-                31,
-                1);
+                "CCC fruits (R)",
+                "70.57",
+                50,
+                7);
         Product productSeven = new Product(
-                7,
+                6,
                 "Bread",
                 482578932,
                 "Kiyv Hlib Zavod (R)",
-                6.78,
+                "6.78",
                 5,
                 1);
         Product productEight = new Product(
-                8,
+                7,
                 "Candy",
                 482987654,
                 "Ne slipnetsa (R)",
-                30.57,
+                "30.57",
                 365,
                 50);
         Product productNine = new Product(
-                9,
+                8,
                 "Cheese",
                 482658713,
                 "Happy Cow (R)",
-                29.99,
+                "29.99",
                 15,
                 6);
         Product productTen = new Product(
-                10,
+                9,
                 "Vodka",
                 "Main Product (R)",
-                60.98);
+                "60.98",
+                3650);
 
         ArrayList<Product> products = new ArrayList<>();
         products.add(productOne);
