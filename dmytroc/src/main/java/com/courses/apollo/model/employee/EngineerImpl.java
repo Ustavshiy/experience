f3419c6767a428f4d0eea3a73417b9edd136356a
chopenko.d@gmail.com <anna03032010>
2017-03-26 21:55:23 +0300

    AP-66 Created Interface IEmployee, class EngineerImpl, class Director.

---
+++ dmytroc/src/main/java/com/courses/apollo/model/employee/EngineerImpl.java
@@ -0,0 +1,30 @@
+package com.courses.apollo.model.employee;
+
+import com.courses.apollo.util.IEmployee;
+
+/**
+ * Abstract class engineer implements IEmployee interface. With basic behavior of engineer.
+ */
+public class EngineerImpl implements IEmployee {
+    /**
+     * Creating, editing and other project method.
+     */
+    public void toProject() {
+
+    }
+
+    @Override
+    public void goToWork() {
+
+    }
+
+    @Override
+    public void receiveSalary() {
+
+    }
+
+    @Override
+    public void takeVacation() {
+
+    }
+}
