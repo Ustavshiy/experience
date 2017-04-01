39e9802ec76a90e94927860e955e7b94a021a5e6
Maestro <maestrobsm@gmail.com>
2017-04-02 00:45:57 +0300

    master-bogdang- Added tests.

--- example/src/main/java/com/courses/apollo/service/DateTimeExample.java
+++ example/src/main/java/com/courses/apollo/service/DateTimeExample.java
@@ -1,23 +1,16 @@
 package com.courses.apollo.service;
 
-import static java.time.temporal.TemporalAdjusters.lastDayOfMonth;
-import static java.time.temporal.TemporalAdjusters.nextOrSame;
-
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.time.DayOfWeek;
-import java.time.Duration;
-import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
-import java.time.chrono.JapaneseDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeFormatterBuilder;
 import java.time.temporal.ChronoField;
 import java.time.temporal.ChronoUnit;
-import java.time.temporal.Temporal;
-import java.time.temporal.TemporalAdjuster;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
@@ -28,104 +21,67 @@ import java.util.Locale;
 public final class DateTimeExample {
 
     /**
-     * Value of year.
-     */
-    private static final int YEAR = 2014;
-
-    /**
-     * Value of month.
-     */
-    private static final int MONTH = 3;
-
-    /**
-     * Value of date.
-     */
-    private static final int DATE = 18;
-
-    /**
-     * Value of hour.
-     */
-    private static final int HRS = 13;
-
-    /**
-     * Value of minute.
-     */
-    private static final int MIN = 45;
-
-    /**
-     * Value of second.
-     */
-    private static final int SEC = 20;
-
-    /**
      * Value of third variant.
      */
     private static final int THREE = 3;
-
-    /**
-     * Value of first number.
-     */
-    private static final int FIRSTNUMB = 44;
-
-    /**
-     * Value of second number.
-     */
-    private static final int SECONDNUMB = 365;
-
-    /**
-     * Value of third number.
-     */
-    private static final int THIRDDNUMB = 86400;
 
     private DateTimeExample() {
 
     }
 
+    /**
+     * Date and time output format.
+     */
+    public static final ThreadLocal<DateFormat> FORMATTERS = new ThreadLocal<DateFormat>() {
+        protected DateFormat initialValue() {
+            return new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
+        }
+    };
 
     /**
      * Old method of using dates.
+     * @param year result format of year.
+     * @param month result format of month.
+     * @param datte result format of date.
+     * @param hrs result format of hours.
+     * @param min result format of minutes.
+     * @return old date format.
      */
-    public static void useOldDate() {
-        Date date = new Date(YEAR, MONTH, DATE, HRS, MIN);
-        System.out.println(new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX").format(date));
+    public static String useOldDate(int year, int month, int datte, int hrs, int min) {
+        Date date = new Date(year, month, datte, hrs, min);
         Calendar calendar = Calendar.getInstance();
-        calendar.set(YEAR, Calendar.FEBRUARY, DATE);
-        System.out.println(calendar);
+        calendar.set(year, Calendar.FEBRUARY, datte);
+        return FORMATTERS.get().format(date);
     }
 
     /**
      * Local method for initializing the date.
+     * @param year result format of year.
+     * @param month result format of month.
+     * @param datte result format of date.
+     * @param hrs result format of hours.
+     * @param min result format of minutes.
+     * @param sec result format of seconds.
+     * @return local date format.
      */
-    public static void useLocalDate() {
-        LocalDate date = LocalDate.of(YEAR, MONTH, DATE);
+    public static LocalDateTime useLocalDate(int year, int month, int datte, int hrs, int min, int sec) {
+        LocalDate date = LocalDate.of(year, month, datte);
         System.out.println(date);
-        LocalTime time = LocalTime.of(HRS, MIN, SEC); // 13:45:20
+        LocalTime time = LocalTime.of(hrs, min, sec); // 13:45:20
         System.out.println(time);
-        LocalDateTime dateTime = LocalDateTime.of(YEAR, Month.MARCH, DATE, HRS, MIN, SEC); // 2014-03-18T13:45
-        System.out.println(dateTime);
-        System.out.println(dateTime.toLocalDate());
-        System.out.println(dateTime.toLocalTime());
-        Instant instant = Instant.ofEpochSecond(FIRSTNUMB * SECONDNUMB * THIRDDNUMB);
-        Instant now = Instant.now();
-        Duration duration = Duration.between(LocalTime.of(HRS, MIN, SEC), time);
-        Duration durationTwo = Duration.between(instant, now);
-        System.out.println(duration.getSeconds());
-        System.out.println(durationTwo.getSeconds());
-        System.out.println(Duration.of(MONTH, ChronoUnit.MINUTES));
-        System.out.println(JapaneseDate.from(date));
+        LocalDateTime dateTime = LocalDateTime.of(year, Month.MARCH, datte, hrs, min, sec); // 2014-03-18T13:45
+        return dateTime;
     }
 
     /**
      * Class for setting time parameters.
+     * @param year result format of year.
+     * @param month result format of month.
+     * @param datte result format of date.
+     * @return local date.
      */
-    public static void useTemporalAdjuster() {
-        LocalDate date = LocalDate.of(YEAR, MONTH, DATE);
-        System.out.println(date.with(nextOrSame(DayOfWeek.SUNDAY)));
-        System.out.println(date.with(lastDayOfMonth()));
-        System.out.println(date.with(new NextWorkingDay()));
-        System.out.println(date.with(nextOrSame(DayOfWeek.FRIDAY)));
-        System.out.println(date.with(new NextWorkingDay()));
-        System.out.println(date.with(nextOrSame(DayOfWeek.FRIDAY)));
+    public static LocalDate useTemporalAdjuster(int year, int month, int datte) {
+        LocalDate date = LocalDate.of(year, month, datte);
         date = date.with(temporal -> {
             DayOfWeek dow = DayOfWeek.of(temporal.get(ChronoField.DAY_OF_WEEK));
             int dayToAdd = 1;
@@ -137,39 +93,18 @@ public final class DateTimeExample {
             }
             return temporal.plus(dayToAdd, ChronoUnit.DAYS);
         });
-        System.out.println(date);
-    }
-
-    /**
-     * The class that returns the next working day.
-     */
-    public static class NextWorkingDay implements TemporalAdjuster {
-        @Override
-        public Temporal adjustInto(Temporal temporal) {
-            DayOfWeek dow = DayOfWeek.of(temporal.get(ChronoField.DAY_OF_WEEK));
-            int dayToAdd = 1;
-            if (dow == DayOfWeek.FRIDAY) {
-                dayToAdd = THREE;
-            }
-            if (dow == DayOfWeek.SATURDAY) {
-                dayToAdd = 2;
-            }
-            return temporal.plus(dayToAdd, ChronoUnit.DAYS);
-        }
+        return date;
     }
 
     /**
      * Class for formatting dates.
+     * @param year result format of year.
+     * @param month result format of month.
+     * @param datte result format of date.
+     * @return complex format.
      */
-    public static void useDateFormatter() {
-        LocalDate date = LocalDate.of(YEAR, MONTH, DATE);
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        DateTimeFormatter italianFormatter = DateTimeFormatter.ofPattern("d. MMMM yyyy", Locale.ITALIAN);
-
-        System.out.println(date.format(DateTimeFormatter.ISO_LOCAL_DATE));
-        System.out.println(date.format(formatter));
-        System.out.println(date.format(italianFormatter));
-
+    public static String useDateFormatter(int year, int month, int datte) {
+        LocalDate date = LocalDate.of(year, month, datte);
         DateTimeFormatter complexFormatter = new DateTimeFormatterBuilder()
                 .appendText(ChronoField.DAY_OF_MONTH)
                 .appendLiteral(". ")
@@ -178,7 +113,6 @@ public final class DateTimeExample {
                 .appendText(ChronoField.YEAR)
                 .parseCaseInsensitive()
                 .toFormatter(Locale.ITALIAN);
-
-        System.out.println(date.format(complexFormatter));
+        return date.format(complexFormatter);
     }
 }
--- example/src/test/java/com/courses/apollo/service/DateTameExampTest.java
+++ example/src/test/java/com/courses/apollo/service/DateTameExampTest.java
@@ -1,16 +1,26 @@
 package com.courses.apollo.service;
 
+import static org.junit.Assert.assertTrue;
+import static com.courses.apollo.service.DateTimeExample.*;
 
 import org.junit.Test;
-
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Modifier;
-
-import static org.junit.Assert.assertTrue;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class DateTameExampTest {
-
+    private final String testData = "3914-04-18T13:45:00.000+03:00";
+    private final String testLocalData = "2014-03-18T13:45:20";
+    private final String testTemporalAdjuster = "2014-03-19";
+    private final String testDateFormatter = "18. marzo 2014";
+    private final Integer year = 2014;
+    private final Integer month = 3;
+    private final Integer date = 18;
+    private final Integer hrs = 13;
+    private final Integer min = 45;
+    private final Integer sec = 20;
 
     @Test
     public void testConstructor()
@@ -21,8 +31,24 @@ public class DateTameExampTest {
         constructor.newInstance();
     }
 
+    @Test
+    public void testOldDate() {
+        Assert.assertEquals(testData, useOldDate(year, month, date, hrs, min));
+    }
 
+    @Test
+    public void testUseLocalDate() {
+        Assert.assertEquals(testLocalData, useLocalDate(year, month, date, hrs, min, sec)
+                .toString());
+    }
 
+    @Test
+    public void testUseTemporalAdjuster() {
+        Assert.assertEquals(testTemporalAdjuster, useTemporalAdjuster(year, month, date).toString());
+    }
 
+    @Test
+    public void testUseDateFormatter() {
+        Assert.assertEquals(testDateFormatter, useDateFormatter(year, month, date).toString());
+    }
 }
-
