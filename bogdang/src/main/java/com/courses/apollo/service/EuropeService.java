e422208286c16428ca1b31542d6e306925e5aa08
Maestro <maestrobsm@gmail.com>
2017-04-03 17:58:56 +0300

    AP-75-bogdang - class EuropeServise completed.

--- bogdang/src/main/java/com/courses/apollo/service/EuropeService.java
+++ bogdang/src/main/java/com/courses/apollo/service/EuropeService.java
@@ -1,16 +1,22 @@
 package com.courses.apollo.service;
 
 import com.courses.apollo.model.europe.Europe;
-import com.sun.tools.javac.util.List;
+import import java.util.List;
 
 import java.util.ArrayList;
 
 /**
- * Created by Богдан on 03.04.2017.
+ * Europe service class.
  */
 public class EuropeService {
-    public List<Europe> territorialDivision( Integer year, List<Europe> europeList) {
-        //Europe.Country result = new Europe().new Country();
+
+    /**
+     * Territorial division of Europe in the required year.
+     * @param year required year.
+     * @param europeList history of division of Europe.
+     * @return result - desired division.
+     */
+    public List<Europe.Country> territorialDivision( Integer year, List<Europe> europeList) {
         List<Europe.Country> result = new ArrayList<>();
         for (int i = 0; i < europeList.size() ; i++) {
             Integer findYear = europeList.get(i).getYear();
