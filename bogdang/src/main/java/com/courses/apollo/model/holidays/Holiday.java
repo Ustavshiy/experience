75d8453f0d74451439008ed818b5c442671077a5
Maestro <maestrobsm@gmail.com>
2017-04-20 23:36:44 +0300

    AP-83-bogdang - rewritten return in findHolidays

--- bogdang/src/main/java/com/courses/apollo/model/holidays/Holiday.java
+++ bogdang/src/main/java/com/courses/apollo/model/holidays/Holiday.java
@@ -17,20 +17,8 @@ public class Holiday {
      */
     private LocalDate date;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public LocalDate getDate() {
         return date;
-    }
-
-    public void setDate(LocalDate date) {
-        this.date = date;
     }
 
     public Holiday(String name, LocalDate date) {
