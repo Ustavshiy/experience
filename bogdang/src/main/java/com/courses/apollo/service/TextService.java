d5850298eb35eb2261e3df418fd29baafe79cb4d
Maestro <maestrobsm@gmail.com>
2017-05-07 23:38:19 +0300

    AP-92-bogdang - Add class TextService

---
+++ bogdang/src/main/java/com/courses/apollo/service/TextService.java
@@ -0,0 +1,27 @@
+package com.courses.apollo.service;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by on 07.05.2017.
+ */
+public class TextService {
+
+    public int findSumNumber(String text){
+        Pattern p = Pattern.compile("-?\\d");
+        Matcher m = p.matcher(text);
+        int count = 0;
+            while (m.find()) {
+                int number = new Integer(m.group());
+                count = count + number;
+            }
+        return count;
+    }
+
+    public String noComments(String text){
+        String replacement = "";
+        String newText = Pattern.compile("/\\*|/").matcher(text).replaceAll(replacement);
+        return newText;
+    }
+}
