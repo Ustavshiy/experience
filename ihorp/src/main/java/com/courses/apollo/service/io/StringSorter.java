e9e16c62459f51741ea3a39ace0ce11ab30deac0
pronko.igor <pronko.igor@gmail.com>
2017-05-14 12:48:24 +0300

    First task done.

--- ihorp/src/main/java/com/courses/apollo/service/io/StringSorter.java
+++ ihorp/src/main/java/com/courses/apollo/service/io/StringSorter.java
@@ -1,51 +1,127 @@
 package com.courses.apollo.service.io;
 
 import java.io.*;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.stream.Stream;
+import java.util.*;
 
 /**
  * Util class.
- * В задании 1 требуется ввести последовательность строк из текстового потока и выполнить указанные действия. При этом могут рассматриваться два варианта:
- • каждая строка состоит из одного слова;
- • каждая строка состоит из нескольких слов.
- Имена входного и выходного файлов, а также абсолютный путь к ним могут
- быть введены как аргументы метода.
  */
 public class StringSorter {
 
 
-
+    /**
+     * WORKING!
+     *
+     * @param input
+     * @param output
+     */
     public static void sortStringInFile(String input, String output) {
 
-        File inputFile = new File(input);
-        File outputFile = new File(output);
-        Writer writeFile = null;
-        Reader readFile = null;
+        BufferedWriter bufferedWriter = null;
+        BufferedReader bufferedReader = null;
         try {
-            writeFile = new FileWriter(outputFile);
-            BufferedWriter bufferedWriter = new BufferedWriter(writeFile);
-            readFile = new FileReader(inputFile);
-            BufferedReader bufferedReader = new BufferedReader(readFile);
+            bufferedWriter = new BufferedWriter(new FileWriter(new File(output), true));
+            bufferedReader = new BufferedReader(new FileReader(new File(input)));
             String line;
-            while((line = bufferedReader.readLine()) != null) {
-                bufferedWriter.write(sortString(line));
+            while ((line = bufferedReader.readLine()) != null) {
+                bufferedWriter.write(sortStringByNumberOfWords(line) + "\n");
             }
         } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            try {
+                bufferedWriter.flush();
+                bufferedWriter.close();
+                bufferedReader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
-    public static String sortString(String line) {
+    /**
+     *
+     * @param text
+     * @return
+     */
+    public static String sortStringByNumberOfWords(String text) {
+        String[] words = text.split("[^A-ZА-Яa-zа-яІіЇїЄє]\\s*\\n*");
+        ArrayList<WordCounter> wordsWithCounter = new ArrayList<>();
+        for (String word : words) {
+            boolean hasWord = false;
+            for (WordCounter wWC : wordsWithCounter) {
+                if (wWC.isMatchWord(word)) {
+                    hasWord = true;
+                }
+            }
+            if (!hasWord) {
+                wordsWithCounter.add(new WordCounter(word, 0));
+            }
+        }
+        Collections.sort(wordsWithCounter, new Comparator<WordCounter>() {
+            @Override
+            public int compare(WordCounter o1, WordCounter o2) {
+                return o1.getCounter().compareTo(o2.getCounter());
+            }
+        });
+        StringBuffer answer = new StringBuffer();
+        for (WordCounter i : wordsWithCounter) {
+            answer.append(i.getWord()).append(" ");
+        }
+        return answer.toString();
+    }
 
-        String[] words = line.split("[^\\w]");
 
-        Stream<String> wordsStream = Arrays.stream(words);
-        Comparator<String> compare = (one, two) -> Integer.compare(one.length(), two.length());
-        String answer = wordsStream.sorted(compare).toString();
-        wordsStream.close();
-        return answer;
+
+    /**
+     * Nested class in order to calculate quantity of words.
+     */
+    private static class WordCounter{
+
+        /**
+         *
+         */
+        private String word;
+
+        /**
+         *
+         */
+        private Integer counter;
+
+        public WordCounter() {}
+
+        public WordCounter(String word, Integer counter) {
+            this.word = word;
+            this.counter = counter;
+        }
+
+        /**
+         *
+         * @param word
+         * @return
+         */
+        public boolean isMatchWord(String word) {
+            if (this.word.toLowerCase().equals(word.toLowerCase())) {
+                counter++;
+                return true;
+            }
+            return false;
+        }
+
+        /**
+         *
+         * @return
+         */
+        public Integer getCounter() {
+            return counter;
+        }
+
+        /**
+         *
+         * @return
+         */
+        public String getWord() {
+            return word;
+        }
     }
 }
-
--- ihorp/src/test/java/com/courses/apollo/service/io/StringSorterTest.java
+++ ihorp/src/test/java/com/courses/apollo/service/io/StringSorterTest.java
@@ -1,11 +1,44 @@
 package com.courses.apollo.service.io;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
-/**
- * Created by Ihor on 09.05.2017.
- */
+import java.io.*;
+
 public class StringSorterTest {
+
+    String testTextFile = "src\\main\\resources\\TestText.txt";
+    String testText = "fifth fifth fifth fifth fifth fourth fourth fourth fourth third third third second second first";
+    String answerTextFile = "src\\main\\resources\\AnswerText.txt";
+    String answerText = "first second third fourth fifth ";
+
+
+    @Before
+    public void generateTestText(){
+        BufferedWriter writeText = null;
+        BufferedWriter writeText2 = null;
+        try {
+            writeText = new BufferedWriter(new FileWriter(new File(testTextFile)));
+            writeText.write(testText);
+            writeText2 = new BufferedWriter(new FileWriter(new File(answerTextFile)));
+            writeText2.write(answerText);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                writeText.flush();
+                writeText2.flush();
+                writeText.close();
+                writeText2.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 
     @Test
     public void testSortStringInFile() {
@@ -16,9 +49,20 @@ public class StringSorterTest {
     }
 
     @Test
-    public void testSortString() {
-        String string = "sdfsdf sdfsdfsdgsd sdfsdgwegdsvsgdsg";
-
-        System.out.println(StringSorter.sortString(string));
+    public void testSortStringByNumberOfWords() {
+//        try {
+//            BufferedReader testReader = new BufferedReader(new InputStreamReader(new FileInputStream(testTextFile), UTF_8));
+//            BufferedReader answerReader = new BufferedReader(new InputStreamReader(new FileInputStream(answerTextFile), UTF_8));
+//            String test = String.valueOf(testReader.read());
+//            String answer = String.valueOf(answerReader.read());
+//            System.out.println(StringSorter.sortStringByNumberOfWords(test));
+//            System.out.println(answer);
+            Assert.assertEquals(StringSorter.sortStringByNumberOfWords(testText), answerText);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        finally {
+//
+//        }
     }
 }
