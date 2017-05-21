0862c232b4f3a9cb16d17f64575f17da4ad7ab03
Maestro <maestrobsm@gmail.com>
2017-05-21 15:43:48 +0300

    AP-92-bogdang - reworked by comments

--- bogdang/src/main/java/com/courses/apollo/service/TextService.java
+++ bogdang/src/main/java/com/courses/apollo/service/TextService.java
@@ -12,22 +12,24 @@ public class TextService {
 
     /**
      * Method for find sum number in text.
+     *
      * @param text - input text.
      * @return count - sum number in text.
      */
     public Integer findSumNumber(String text) {
         Pattern pattern = Pattern.compile("\\d");
         Matcher matcher = pattern.matcher(text);
-        Integer count = 0;
-            while (matcher.find()) {
-                Integer number = Integer.valueOf(matcher.group());
-                count += number;
-            }
+        int count = 0;
+        while (matcher.find()) {
+            int number = Integer.valueOf(matcher.group());
+            count += number;
+        }
         return count;
     }
 
     /**
      * Removes all comments from text.
+     *
      * @param text - input text.
      * @return text without comments.
      */
@@ -39,6 +41,7 @@ public class TextService {
 
     /**
      * Find one word which is not repeats.
+     *
      * @param text - input text.
      * @return found word.
      */
@@ -46,7 +49,7 @@ public class TextService {
         List<String> words = Arrays.asList(text.split("\\W+"));
         Integer quantity = words.size();
         String wordResult = "No coincidence";
-        Integer result = 0;
+        int result = 0;
         for (String comparable : words) {
             for (String word : words) {
                 if (!comparable.equalsIgnoreCase(word)) {
@@ -63,6 +66,7 @@ public class TextService {
 
     /**
      * Find sum even numeral in text.
+     *
      * @param text - input text.
      * @return count sum even numeral.
      */
@@ -71,8 +75,8 @@ public class TextService {
         Matcher matcher = pattern.matcher(text);
         int count = 0;
         while (matcher.find()) {
-            Integer number = Integer.valueOf(matcher.group());
-            Integer remainder = number % 2;
+            int number = Integer.valueOf(matcher.group());
+            int remainder = number % 2;
             if (remainder == 0) {
                 count += number;
             }
@@ -82,6 +86,7 @@ public class TextService {
 
     /**
      * Remove space in text.
+     *
      * @param text - input text.
      * @return text without space.
      */
