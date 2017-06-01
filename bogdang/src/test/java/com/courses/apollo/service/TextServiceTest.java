4011333ef2bd0b37dad5091a1f19fc8b793b6f9e
MaestroB <maestrobsm@gmail.com>
2017-06-01 19:22:30 +0300

    AP-92  refactor class TextServiceTest

--- bogdang/src/test/java/com/courses/apollo/service/TextServiceTest.java
+++ bogdang/src/test/java/com/courses/apollo/service/TextServiceTest.java
@@ -2,7 +2,11 @@ package com.courses.apollo.service;
 
 import com.courses.apollo.model.TextTestData;
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TextServiceTest {
 
@@ -12,29 +16,42 @@ public class TextServiceTest {
     private static final String word = "and";
     private static final Integer QUANTITY_EVEN_NUMERAL = 140;
 
+    private static List<String> text = new ArrayList<>();
+    private static List<Integer> quantity = new ArrayList<>();
+
+    @BeforeClass
+    public static void onlyOne(){
+        text.add(new String (textTestData.expectedTextTwo()));
+        text.add(new String (textTestData.getTextThree()));
+        text.add(new String (textTestData.getTextFive()));
+        quantity.add(new Integer (textTestData.getTextOne()));
+        quantity.add(new Integer (textTestData.getTextFour()));
+
+    }
+
     @Test
     public void testFindSumNumber(){
-        Assert.assertEquals(QUANTITY_NUMBER, textService.findSumNumber(textTestData.getTextOne()));
+        Assert.assertEquals(QUANTITY_NUMBER, quantity.get(0));
     }
 
     @Test
     public void testNoComments(){
-        Assert.assertEquals(textTestData.expectedTextTwo(),textService.removeComments(textTestData.getTextTwo()));
+        Assert.assertEquals(textTestData.expectedTextTwo(), text.get(0));
     }
 
     @Test
     public void testFindOneWord(){
-        Assert.assertEquals(word, textService.findOneWord(textTestData.getTextThree()));
+        Assert.assertEquals(word, text.get(1));
     }
 
     @Test
     public void testFindSumEvenNumeral(){
-        Assert.assertEquals(QUANTITY_EVEN_NUMERAL, textService.findSumEvenNumeral(textTestData.getTextFour()));
+        Assert.assertEquals(QUANTITY_EVEN_NUMERAL, quantity.get(1));
     }
 
     @Test
     public void textRemoveSpace(){
-        Assert.assertEquals(textTestData.expectedTextFive(),textService.removeSpace(textTestData.getTextFive()));
+        Assert.assertEquals(textTestData.expectedTextFive(), text.get(2));
     }
 
 }
