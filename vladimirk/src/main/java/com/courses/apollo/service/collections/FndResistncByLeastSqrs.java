a57deff0f909ce80c7546f8982d4863433b3c6fa
vladimir kramar <kramarjob@gmail.com>
2017-06-12 10:25:59 +0300

    AP-109 Wrapper classes in method fndResistncByLeastSqrs replaced with primitives.

--- vladimirk/src/main/java/com/courses/apollo/service/collections/FndResistncByLeastSqrs.java
+++ vladimirk/src/main/java/com/courses/apollo/service/collections/FndResistncByLeastSqrs.java
@@ -11,12 +11,12 @@ public class FndResistncByLeastSqrs {
      * Method for finding network resistance from a set of measurements of amperage and voltage by least squares.
      */
     public Double fndResistncByLeastSqrs(Queue<Double> amperage, Queue<Double> voltage) {
-        Double sumNumerator = 0.0;
-        Double sumDenominator = 0.0;
+        double sumNumerator = 0.0;
+        double sumDenominator = 0.0;
         if (!amperage.isEmpty() && amperage.size() == voltage.size()) {
             while (!amperage.isEmpty()) {
-                Double amp = amperage.poll();
-                Double volt = voltage.poll();
+                double amp = amperage.poll();
+                double volt = voltage.poll();
                 sumNumerator += amp * volt;
                 sumDenominator += amp * amp;
             }
